2059abbb6b84cc296c7d6afe839d8947f9b81397
Miroslav Matejovsky <miroslav.matejovsky@wirecard.com>
2018-12-05 18:05:34 +0100

    day 1 part 2

--- 2018/01/src/main/kotlin/Year_2018_01.kt
+++ 2018/01/src/main/kotlin/Year_2018_01.kt
@@ -2,10 +2,28 @@ fun main(args: Array<String>) {
 
   val lines = ClassLoader::class.java.getResource("/input.txt").readText().lines()
 
-  val summed = lines
+  val changes = lines
     .filter { line -> line.isNotBlank() }
     .map { number -> number.toInt() }
-    .sum()
 
-  println("Sum is $summed")
+  println("Sum is ${changes.sum()}")
+
+  tailrec fun <T> repeat(acc: List<T>, list: List<T>, times: Int): List<T> = if (times <= 0) acc else repeat(acc + list, list, times - 1)
+
+  fun <T> repeat(list: List<T>, times: Int): List<T> =
+    repeat(emptyList(), list, times)
+
+//  val repeatedChanges = repeat(changes, 1)
+  val repeatedChanges = repeat(changes, 150)
+  val frequencies = repeatedChanges
+    .fold(listOf(0)) { list, change ->
+      list + (list.last() + change)
+    }
+//  println("frequencies is $frequencies")
+
+  val counts = frequencies.groupingBy{ it }
+    .eachCount()
+    .filter { entry -> entry.value > 1 }
+
+  println("counts is $counts")
 }
